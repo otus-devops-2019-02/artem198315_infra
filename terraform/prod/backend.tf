2d3d755b3a624bb27a2162cdae34da9f9ffa1698
Artem Ustinov <artem198315@gmail.com>
2019-04-24 00:03:39 +0400

    add storage-bucket && remote backend

---
+++ terraform/prod/backend.tf
@@ -0,0 +1,6 @@
+terraform {
+  backend "gcs" {
+    bucket  = "gs://storage-bucket-artem198315-prod"
+    prefix  = "terraform/prod"
+  }
+}
---
+++ terraform/stage/backend.tf
@@ -0,0 +1,6 @@
+terraform {
+  backend "gcs" {
+    bucket  = "storage-bucket-artem198315-stage"
+    prefix  = "terraform/stage"
+  }
+}
