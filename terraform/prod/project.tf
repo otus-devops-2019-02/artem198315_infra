bd4c22f2ed1521f35199d7d937539167517b2fc8
Artem Ustinov <artem198315@gmail.com>
2019-04-27 12:22:34 +0400

    Hw 8

--- terraform/prod/project.tf
+++ terraform/prod/project.tf
@@ -1,4 +1,4 @@
 resource "google_compute_project_metadata_item" "ssh-keys" {
   key   = "ssh-keys"
-  value = "shinta:${file("${var.public_key_path}")}appuser1:${file("${var.public_key_path}")}appuser2:${file("${var.public_key_path}")}"
+  value = "shinta:${file("${var.public_key_path}")}appuser:${file("${var.public_key_path}")}appuser2:${file("${var.public_key_path}")}"
 }
--- terraform/stage/outputs.tf
+++ terraform/stage/outputs.tf
@@ -1,3 +1,10 @@
 output instance_ext_ip_addr {
   value = "${module.app.ext_ip_addr}"
 }
+
+output instance_ext_db_addr {
+  value = "${module.db.ext_db_addr}"
+}
+  
+
+
